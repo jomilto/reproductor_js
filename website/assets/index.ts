de28afd88e9b5c1a5f78ac6bd3a364a3c4727e32
JLob Lap <joselobatof@gmail.com>
2020-12-12 01:03:06 -0600

    importando proyecto desde npm

--- website/assets/index.ts
+++ website/assets/index.ts
@@ -1,7 +1,7 @@
-import MediaPlayer from '../mediaplayer/MediaPlayer';
-import AutoPlay from './plugins/AutoPlay';
-import AutoPause from './plugins/AutoPause';
-import Ads from './plugins/Ads';
+import MediaPlayer from '@jomilto/platzi-mediaplayer';
+import AutoPlay from '@jomilto/platzi-mediaplayer/lib/plugins/AutoPlay';
+import AutoPause from '@jomilto/platzi-mediaplayer/lib/plugins/AutoPause';
+import Ads from '@jomilto/platzi-mediaplayer/lib/plugins/Ads';
 
 const video = document.querySelector("video");
 const player = new MediaPlayer({ el: video, plugins: [
